df9772e2385e5867adcebcbbbe343e592ba6db99
matt manes <mattmanes@pm.me>
2023-03-31 19:10:24 -0500

    feat: add size property

--- src/pathier/pathier.py
+++ src/pathier/pathier.py
@@ -21,6 +21,17 @@ class Pathier(pathlib.Path):
             )
         return self
 
+    @property
+    def size(self) -> int | None:
+        """The size in bytes of this file or directory.
+        Returns None if this path doesn't exist."""
+        if not self.exists():
+            return None
+        if self.is_file():
+            return self.stat().st_size
+        if self.is_dir():
+            return sum(file.stat().st_size for file in self.rglob("*.*"))
+
     def moveup(self, name: str) -> Self:
         """Return a new Pathier obj that is a parent of this instance.
 
